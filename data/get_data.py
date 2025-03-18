bf6d8d9e0bc780168aeeeb36c01cdf1189f2b52d
Syyyk <shenyikai1101@gmail.com>
2025-03-18 14:12:25 +0800

    Align get_data column name

--- data/get_data.py
+++ data/get_data.py
@@ -25,10 +25,10 @@ def get_daily_ret_statement(start_date: Union[str, int], end_date: Union[str, in
     ddb_statement = rf"""
     db = database("dfs://CSMAR")
     pt = loadTable(db,`Daily)
-    select date as trade_date, id as stock_code, close, volume,
-    (close - prev(close)) / prev(close) as ret from pt
-    where date >= {start_date} and date <= {end_date} {id_filter}
-    context by id csort date
+    select TradeDate, InstrumentID, Close, Volume,
+    (Close - prev(Close)) / prev(Close) as ret from pt
+    where TradeDate >= {start_date} and TradeDate <= {end_date} {id_filter}
+    context by InstrumentID csort TradeDate
     """
     return ddb_statement
 
@@ -39,10 +39,10 @@ def get_30min_ret_statement(start_date: Union[str, int], end_date: Union[str, in
     ddb_statement = rf"""
     db = database("dfs://MinFreq")
     pt = loadTable(db,`baostock_30min)
-    select time, id, close, volume,
-    (close - prev(close)) / prev(close) as ret from pt
-    where date(time) >= {start_date} and date(time) <= {end_date} {id_filter}
-    context by id csort time
+    select TradeDate, TradeTime, InstrumentID, Close, Volume,
+    (Close - prev(Close)) / prev(Close) as ret from pt
+    where TradeDate >= {start_date} and TradeDate <= {end_date} {id_filter}
+    context by InstrumentID csort TradeDateTime
     """
     return ddb_statement
 
@@ -53,9 +53,9 @@ def get_5min_ret_statement(start_date: Union[str, int], end_date: Union[str, int
     ddb_statement = rf"""
     db = database("dfs://MinFreq")
     pt = loadTable(db,`baostock_5min)
-    select time, id, close, volume,
-    (close - prev(close)) / prev(close) as ret from pt
-    where date(time) >= {start_date} and date(time) <= {end_date} {id_filter}
-    context by id csort time
+    select TradeDate, TradeTime, InstrumentID, Close, Volume,
+    (Close - prev(Close)) / prev(Close) as ret from pt
+    where TradeDate >= {start_date} and TradeDate <= {end_date} {id_filter}
+    context by InstrumentID csort TradeDateTime
     """
     return ddb_statement
